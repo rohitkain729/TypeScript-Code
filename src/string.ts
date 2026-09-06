31139db3ab178aacff2e9bb288e31804619462d1
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-06 13:50:16 +0530

    undefined explained in code

--- src/string.ts
+++ src/string.ts
@@ -1,13 +1,15 @@
-// let firstname:string="rohit k"
-// let lastname:string='verma'
-// let age:number = 34;
-// let course:string =`hi i am ${firstname} ${lastname} age is ${age} and i am learning TS`
 
-// console.log(course.toUpperCase().length);
+let username:string;
+console.log(username);  // undefined
 
-// boolean
-let isloggedIn:boolean = true;
-console.log(isloggedIn);
+function greet(){
+    console.log("hello");
+}
+
+let result = greet();
+console.log(result);  // undefined
+
+
 
 
 
