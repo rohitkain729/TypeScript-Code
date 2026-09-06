d24737b670aed12133f7b3a665175aea69b1cb02
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-06 13:41:59 +0530

    sdded the string code

---
+++ src/string.ts
@@ -0,0 +1,16 @@
+// let firstname:string="rohit k"
+// let lastname:string='verma'
+// let age:number = 34;
+// let course:string =`hi i am ${firstname} ${lastname} age is ${age} and i am learning TS`
+
+// console.log(course.toUpperCase().length);
+
+// boolean
+let isloggedIn:boolean = true;
+console.log(isloggedIn);
+
+
+
+
+
+
