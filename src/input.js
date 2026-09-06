47c01701e31cce7f47aa3b50b01a4ce4617aec3d
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-06 18:44:48 +0530

    add the inference code on type

---
+++ src/input.js
@@ -0,0 +1,14 @@
+"use strict";
+let course = "TS";
+course = "JS";
+console.log(course);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 5));
+function greet(name) {
+    return name;
+}
+const name2 = greet("rohit");
+console.log(name2);
+let user;
