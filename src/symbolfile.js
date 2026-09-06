d8984c205a8e6137a4ee20e2822608c505ebbaa6
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-06 17:42:14 +0530

    symbol code

---
+++ src/symbolfile.js
@@ -0,0 +1,19 @@
+"use strict";
+// let id1= Symbol();
+// let id2 = Symbol();
+let id1 = Symbol("id"); // symbol with description
+let id2 = Symbol("id");
+// let user = {
+//     [id1]:1,
+//     [id2]:2
+//    }
+// let token:symbol = Symbol("token");
+// symbol imuatable ,unique  , it take primitive data ty[pe
+let obj = {
+    id: 1
+};
+const id3 = Symbol("id");
+let obj2 = {
+    [id3]: 1
+};
+console.log(Symbol("id").toString());
