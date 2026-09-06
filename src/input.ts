a48bc6a8a0a5a6e3e4cdf2ddaea96e58e5fa2e0a
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-07 00:57:51 +0530

    intersection with object

--- src/input.ts
+++ src/input.ts
@@ -1,24 +1,28 @@
-// union types
-// multiple handler
-let id: string|number;  // union
-id=101;
-id="rohit";
-// id=true; // not valid
-console.log(id);
-
-function printId(id:string|number){
-console.log(id);
+type Person ={
+    username:string
 }
-printId(100);
-printId("preety");
 
-//type narrowing
-function printData(data: string|number){
-    if(typeof data === "string"){
-        console.log(data.toUpperCase());
-    }else{
-        console.log(data);
-    }
+type  Emp = {
+    salary:number
 }
-printData("rohit");
-printData(44);
+
+type staff = Person & Emp;
+
+
+type User = {
+    username:string
+}
+
+type Admin = {
+  permission :string [] 
+}
+type AdminUser = User & Admin;
+
+let admin: AdminUser = {
+    username :"rohit",
+    permission : ["read","write"]
+}
+console.log(admin);
+
+
+
