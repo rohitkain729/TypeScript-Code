548fe413d3136783288d2ac6aa98a14933a792e8
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-07 01:02:39 +0530

    intersection with interface

--- src/input.ts
+++ src/input.ts
@@ -1,28 +1,16 @@
-type Person ={
-    username:string
+interface Person {
+    username:string,
 }
 
-type  Emp = {
-    salary:number
+interface Emp{
+   salary :number 
 }
 
-type staff = Person & Emp;
+type staff = Person &Emp;
 
-
-type User = {
-    username:string
+let workerSaff :staff = {
+    username : "rohit",
+    salary : 483000
 }
 
-type Admin = {
-  permission :string [] 
-}
-type AdminUser = User & Admin;
-
-let admin: AdminUser = {
-    username :"rohit",
-    permission : ["read","write"]
-}
-console.log(admin);
-
-
-
+console.log(workerSaff);
