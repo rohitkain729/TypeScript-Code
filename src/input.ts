64446ff1266a5e8ac67c76f91b1fc0195835cf87
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-06 21:15:47 +0530

    tuple and array comparaitive study and use

--- src/input.ts
+++ src/input.ts
@@ -1,55 +1,16 @@
-// arrays
-
-let number = [10,30,40];
-
-console.log(number);
-
-let marks:number[] = [90,32,67];
-console.log(marks);
-
-// Array<Type>
-
-let user : Array<string> = [
-    "Rohit",
-    "Verma"
-]
+// tuples
+let user:[string,number] = ["rohit",34];
 console.log(user);
 
-
-let price:number[] = [100,3403,438];
-let cities:string[] = ["Delhi","Mumbai"];
-let statusCode:boolean[] = [true,false,true];
-
-
-//push pop
-
-price.push(444);
-console.log(price);
-
-let fruit:string[] = ["Apple"];
-fruit.push("mango");
-console.log(fruit);
-console.log(fruit.length);
-console.log(fruit.indexOf("mango"));
+let numbersData:number[] = [1,2,3,4];
+let empData:[string,number]=["rohit",34000];
+let [username,salary] = empData;
+console.log(username);
+console.log(salary);
 
 
-let data:(string | number)[]= [
-    "rohit",
-    24,
-    "delhi"
-]
-data.push("meeta");
-data.push("Jag jivan nagar");
-console.log(data);
+function getUser():[string,number]{
+    return ["rohit",343000]
+}
 
-
-
-
-
-
-
-
-
-
-
-
+console.log((getUser()));
