6034d88ff099bbac68f3cff566178a8a15692f69
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-06 19:29:09 +0530

    add the array code first commit

--- src/input.ts
+++ src/input.ts
@@ -1,19 +1,55 @@
-const inputField = document.getElementById("username") as HTMLInputElement;
+// arrays
+
+let number = [10,30,40];
+
+console.log(number);
+
+let marks:number[] = [90,32,67];
+console.log(marks);
+
+// Array<Type>
+
+let user : Array<string> = [
+    "Rohit",
+    "Verma"
+]
+console.log(user);
 
 
-const button = document.getElementById("btn") as HTMLButtonElement;
+let price:number[] = [100,3403,438];
+let cities:string[] = ["Delhi","Mumbai"];
+let statusCode:boolean[] = [true,false,true];
 
-button.addEventListener("click",()=>{
-   
-const value = inputField.value;
 
-if(value.trim()===""){
-    alert("please enetr name");
-}else{
-    console.log(value);
-    
-}
+//push pop
 
-})
+price.push(444);
+console.log(price);
+
+let fruit:string[] = ["Apple"];
+fruit.push("mango");
+console.log(fruit);
+console.log(fruit.length);
+console.log(fruit.indexOf("mango"));
+
+
+let data:(string | number)[]= [
+    "rohit",
+    24,
+    "delhi"
+]
+data.push("meeta");
+data.push("Jag jivan nagar");
+console.log(data);
+
+
+
+
+
+
+
+
+
+
 
 
