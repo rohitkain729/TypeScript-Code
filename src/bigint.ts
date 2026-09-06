246e222f01dfc5bac0374d6fdaded41d10663684
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-06 16:59:07 +0530

    bigint code

---
+++ src/bigint.ts
@@ -0,0 +1,23 @@
+// Big int data type
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+//  to make no bigint
+// let bigNumber = 32133434325425425n;
+// let bigNumber = BigInt(478573489);
+
+// let bigNumber :bigint = BigInt(478573489);
+// console.log(bigNumber);
+
+let a:bigint = 100n;
+let b:bigint = 5n;
+console.log(a*b);
+console.log(a/b);
+console.log(a+b);
+console.log(a-b);
+
+
+
+
+
+
