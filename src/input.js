609a52fd996868adebacfe34a8b5392a82fb7b50
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-06 21:03:17 +0530

    map,filter,reduce used

--- src/input.js
+++ src/input.js
@@ -1,14 +1,15 @@
 "use strict";
-let course = "TS";
-course = "JS";
-console.log(course);
-function add(a, b) {
-    return a + b;
-}
-console.log(add(2, 5));
-function greet(name) {
-    return name;
-}
-const name2 = greet("rohit");
-console.log(name2);
-let user;
+let number2 = [1, 2, 3, 4];
+let double = number2.map(num => num * 2);
+console.log(double); // [2,4,...]
+let number = [10, 20, 30, 40];
+const numberdata = number.filter((num) => {
+    return num > 19;
+});
+console.log(numberdata);
+const user = ["rohit", "ashok", "ravi", "abhi", "rama"];
+const userData = user.filter((u) => { return u.startsWith("r"); });
+console.log(userData);
+let totalPrice = [100, 200, 300];
+const totalSum = totalPrice.reduce((sum, price) => { return (sum + price); }, 0);
+console.log(totalSum);
