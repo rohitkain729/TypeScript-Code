499ac66d6609aeb824b41cc80ef1b120689b56b0
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-07 01:50:35 +0530

    arrowfunction

--- src/input.ts
+++ src/input.ts
@@ -1,40 +1,11 @@
-function greet(username:string):void{
-    console.log(`hi  ${username}`);
-}
-// greet("rohit");
+const addNumber = (a:number,b:number):number=>{return a+b};
+
+const sumdata = addNumber(2,3);
+
+console.log(sumdata);
 
 
-function greet2(username:string):string{
-    return `hi ${username}`;
-}
-const result= greet2("rohit");
+const square = (a:number):number=>{return a*a};
 
-console.log(result);
-
-//optional parameter
-function  greetUser(username?:string){
-    console.log(username);
-}
-
-function greetuser2(
-    age?:number,
-    username?:string
-){
- console.log(age,username);    
-}
-greetuser2(24,"rohit");
-
-// default parameter
-function greetuser3(
-    age: number =54,
-    username:string = "ashok"
-){
- console.log(age,username);    
-}
-greetuser3();
-
-
-
-
-
-
+const res =square(2);
+console.log(res);
