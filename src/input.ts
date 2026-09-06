faf30fd5cf2c20e005b7c0975e4de8f6aeb8efb9
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-07 01:44:53 +0530

    optional paramter and default parameter

--- src/input.ts
+++ src/input.ts
@@ -1,16 +1,40 @@
-interface Person {
-    username:string,
+function greet(username:string):void{
+    console.log(`hi  ${username}`);
+}
+// greet("rohit");
+
+
+function greet2(username:string):string{
+    return `hi ${username}`;
+}
+const result= greet2("rohit");
+
+console.log(result);
+
+//optional parameter
+function  greetUser(username?:string){
+    console.log(username);
 }
 
-interface Emp{
-   salary :number 
+function greetuser2(
+    age?:number,
+    username?:string
+){
+ console.log(age,username);    
 }
+greetuser2(24,"rohit");
 
-type staff = Person &Emp;
-
-let workerSaff :staff = {
-    username : "rohit",
-    salary : 483000
+// default parameter
+function greetuser3(
+    age: number =54,
+    username:string = "ashok"
+){
+ console.log(age,username);    
 }
+greetuser3();
 
-console.log(workerSaff);
+
+
+
+
+
