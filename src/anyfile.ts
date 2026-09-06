9fede32d79db6ca56d46e9d94e2b698bbd141bf6
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-06 15:26:01 +0530

    any ,unkown use

---
+++ src/anyfile.ts
@@ -0,0 +1,24 @@
+
+// ANY TYPE
+// it can take any type of data
+// let data:any = "rohit";
+// console.log(data);
+// data = 123;
+// console.log(data);
+// data=true
+// console.log(data);
+
+
+// let value:any = "hi";
+// value = 123;
+// console.log(value.toUpperCase());
+
+// unknown type
+
+// let value:unknown = "TS";
+// if(typeof value === "string"){
+//     console.log(value.toUpperCase());
+// }
+
+
+
