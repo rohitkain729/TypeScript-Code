61e8fdc163e01abfe3a9f23bf9167b464bde7ae5
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-08 04:55:17 +0530

    type narrowing in class and type

--- src/input.ts
+++ src/input.ts
@@ -1,25 +1,76 @@
-// Index Signature
-interface UserRoles{
-    [key:string]:string
+// Type Narrowing
+
+let value:string | number ;
+
+// value = "Mohit" ;
+// value = 100;
+// if(typeof value === "string"){
+//     console.log(value.toUpperCase());
+// }else{
+//     console.log(value);
+// }
+
+function printVal(value : string |number){
+ if(typeof value === "string"){
+    console.log(value.toUpperCase());
+}else{
+    console.log(value.toFixed(2));
+}
+}
+printVal("rohit");
+printVal(2);
+
+type Admin = {
+    name:string,
+    permission:string[]
 }
 
-// dynamic keys
-interface Languages {
-    [key:string]:string
+type User = {
+    name:string,
+    email:string
 }
 
-let LanguagesObj:Languages={
-    en:"English",
-    hi:"hindi",
-    fr:"french"
+function getInfo(person:Admin|User) {
+    if("permission" in person){
+        console.log("admin name is "+person.name);
+    }else{
+        console.log("user name is "+person.name);
+    }
 }
 
-console.log(LanguagesObj);
+getInfo({
+    name:"rohit",
+    permission : ["edit","update"]
+})
+
+getInfo({
+    name:"priya",
+    email : "priya@gmail.com"
+})
+
+// instance of class
+
+class Dog{
+    bark(){
+        console.log('dog is bark');
+    }
+}
+
+class  Cat{
+    meow(){
+console.log('Cat is meow');
+
+    }
+}
 
 
+function MakeSound(animal:Dog|Cat){
+ if(animal instanceof Dog){
+    animal.bark();
+ }else{
+    animal.meow();
+ }
+}
 
-
-
-
-
-
+MakeSound(new Dog);
+MakeSound(new Cat);
