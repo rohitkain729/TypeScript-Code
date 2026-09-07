d421e0dac3cd1b232b9543ff495c7a237c32adf1
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-07 12:47:52 +0530

    add code of rest and spread

--- src/input.ts
+++ src/input.ts
@@ -1,13 +1,36 @@
-// paramater ? : type
+// .... spread or rest
 
-function welcome(
-    age?:number,
-    username:string = "rohit",
-    country?:string
-){
-    console.log(`hi bro ${username} ${age} ${country}`);
+// rest paramater
+function addNumber(...num:number[]){
+    console.log(num);   
 }
+addNumber(10,30,30.22,45);
 
-welcome(56,"ashok","india");
+function  totalPrice(name?:string,
+    ...prices:number[]
+){
+     return `${prices.reduce((sum,price)=>{return sum+price},0)} ${name}`
+}
+console.log(totalPrice("amit",10,202,303));
+
+// spread operator
+let num = [1,3,4,5];
+let num2 = [...num,2,4];
+console.log(num2);
+
+// spread in object
+let user = {
+    name:"rohit",
+    age:23
+}
+let updateUSer={...user,age:35,country:"india"};
+console.log(updateUSer);
+
+
+
+
+
+
+
 
 
