48cd4a87c02b42517dea79835e341ba92f4fe934
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-08 05:08:02 +0530

    literal type code

--- src/input.ts
+++ src/input.ts
@@ -1,76 +1,30 @@
-// Type Narrowing
 
-let value:string | number ;
+// let role:string;
+// role="rohit";
+// console.log(role);
 
-// value = "Mohit" ;
-// value = 100;
-// if(typeof value === "string"){
-//     console.log(value.toUpperCase());
-// }else{
-//     console.log(value);
-// }
+type UserRole = "admin" | "user" | "guest";
 
-function printVal(value : string |number){
- if(typeof value === "string"){
-    console.log(value.toUpperCase());
-}else{
-    console.log(value.toFixed(2));
+let role:UserRole = "admin";
+
+console.log(role);
+
+
+type DiceValue = 1|2|3|4|5|6;
+
+let DicSelected :DiceValue  =1;
+console.log(DicSelected);
+
+let isVeryFied :true;
+isVeryFied = true;
+console.log(isVeryFied);
+
+//  function
+function setTheme(theme: "light" | "dark";){
+    console.log(theme);
 }
-}
-printVal("rohit");
-printVal(2);
-
-type Admin = {
-    name:string,
-    permission:string[]
-}
-
-type User = {
-    name:string,
-    email:string
-}
-
-function getInfo(person:Admin|User) {
-    if("permission" in person){
-        console.log("admin name is "+person.name);
-    }else{
-        console.log("user name is "+person.name);
-    }
-}
-
-getInfo({
-    name:"rohit",
-    permission : ["edit","update"]
-})
-
-getInfo({
-    name:"priya",
-    email : "priya@gmail.com"
-})
-
-// instance of class
-
-class Dog{
-    bark(){
-        console.log('dog is bark');
-    }
-}
-
-class  Cat{
-    meow(){
-console.log('Cat is meow');
-
-    }
-}
+setTheme("dark");
 
 
-function MakeSound(animal:Dog|Cat){
- if(animal instanceof Dog){
-    animal.bark();
- }else{
-    animal.meow();
- }
-}
 
-MakeSound(new Dog);
-MakeSound(new Cat);
+
