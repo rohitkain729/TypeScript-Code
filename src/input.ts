1b06974aefa70a2eed53253b14e8447f2f9f720f
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-08 04:33:48 +0530

    Index Signature Code

--- src/input.ts
+++ src/input.ts
@@ -1,65 +1,20 @@
-//  type vs interface
-
-type User = {
-    username:string,
-    email:string
-}
-let user1:User = {
-    username:"rohit",
-    email:"rohit@gmail.com"
-}
-let user2:User = {
-    username:"ravi",
-    email:"ravi@gmail.com"
-}
-console.log(user1.username);
-console.log(user2.username);
-
-type ID = string|number;
-
-type Admin = User & {
-    role:string
+// Index Signature
+interface UserRoles{
+    [key:string]:string
 }
 
-type Add = (
-    a:number,
-    b:number
-)=> number;
-
-
-interface user{
-    userName:string,
+// dynamic keys
+interface Languages {
+    [key:string]:string
 }
 
-interface Admin2 extends User {
- role:string
+let LanguagesObj:Languages={
+    en:"English",
+    hi:"hindi",
+    fr:"french"
 }
 
-
-// interface
-
-interface Product {
-    title:string,
-    price:number
-}
-let laptop:Product = {
-    title:"macbook",
-    price:340000
-}
-console.log(laptop);
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(LanguagesObj);
 
 
 
