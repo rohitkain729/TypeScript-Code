6754ba868da051fbdd410d51bbe98b3802e527e3
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-08 04:18:01 +0530

    type and interface using extends

--- src/input.ts
+++ src/input.ts
@@ -17,6 +17,54 @@ console.log(user2.username);
 
 type ID = string|number;
 
+type Admin = User & {
+    role:string
+}
+
+type Add = (
+    a:number,
+    b:number
+)=> number;
+
+
+interface user{
+    userName:string,
+}
+
+interface Admin2 extends User {
+ role:string
+}
+
+
+// interface
+
+interface Product {
+    title:string,
+    price:number
+}
+let laptop:Product = {
+    title:"macbook",
+    price:340000
+}
+console.log(laptop);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
