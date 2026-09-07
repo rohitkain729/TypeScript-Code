ca9d8930a40c588b2e1b09c21a10831463b47b90
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-07 17:40:50 +0530

    inteface function code with reusablilty

--- src/input.ts
+++ src/input.ts
@@ -1,23 +1,37 @@
-interface Employee
-{
-    id:number,
-    username?:string,
-    salary?:number
+//  interface with function
+
+interface Multiply{
+   (a?:number,b?:number):number
 }
 
+let multpFn :Multiply=(a,b)=>{return a*b};
 
-let emplyee : Employee = {
-    id:1,
-    username:"rohit",
-    salary:320023
+console.log(multpFn(2,4));
+interface Greeting {
+    (username:string):string
+}
+let greeFn:Greeting = (username)=>{return username };
+console.log(greeFn("hi"));
+
+//  resuable function contracts
+
+interface Calcualor{
+    (a:number,b:number) : number
 }
 
-let emplyee2 : Employee = {
-    id:133,
-    username:"Kohit",
-    salary:32008823
-}
-console.log(emplyee2.salary);
-console.log(emplyee);
+let addFn:Calcualor=(a,b)=>{return a+b};
+let mulFn:Calcualor=(a,b)=>{return a*b};
+let divFn:Calcualor=(a,b)=>{return a/b};
+
+
+console.log(addFn(2,5));
+console.log(mulFn(2,5));
+console.log(divFn(20,5));
+
+
+
+
+
+
 
 
